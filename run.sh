f7217cb994ee74bb8a4ad2292222522612849ef1
Johannes Hötter <johannes.hoetter@kern.ai>
2022-10-30 12:35:03 +0100

    add truss and upload directory as zip

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 /usr/bin/curl -s "$1" > input.json;
 /usr/bin/curl -s echo "$2" >> active_transfer_learning.py;
 /usr/bin/curl -s "$3" > embedding.csv.bz2;
-/usr/local/bin/python run_ml.py "$4";
+/usr/local/bin/python run_ml.py "$4" "$5";
--- run_ml.py
+++ run_ml.py
@@ -2,11 +2,12 @@
 import sys
 import util
 import requests
-from collections import defaultdict
 import pandas as pd
+import truss
+import shutil
 
 CONSTANT__OUTSIDE = "OUTSIDE"  # enum from graphql-gateway; if it changes, the extraction service breaks!
-
+TRUSS_DIR = "truss_model"
 
 def run_classification(corpus_embeddings, corpus_labels, corpus_ids, training_ids):
     from active_transfer_learning import ATLClassifier
@@ -37,6 +38,7 @@ def run_classification(corpus_embeddings, corpus_labels, corpus_ids, training_id
             )
     if len(ml_results_by_record_id) == 0:
         print("No records were predicted. Try lowering the confidence threshold.")
+    truss.mk_truss(classifier.model, target_directory=TRUSS_DIR)
     return ml_results_by_record_id
 
 
@@ -48,7 +50,9 @@ def run_extraction(corpus_embeddings, corpus_labels, corpus_ids, training_ids):
         corpus_embeddings, corpus_labels, corpus_ids, training_ids
     )
     ml_results_by_record_id = {}
-    for record_id, prediction, probability in zip(corpus_ids, predictions, probabilities):
+    for record_id, prediction, probability in zip(
+        corpus_ids, predictions, probabilities
+    ):
         df = pd.DataFrame(
             list(zip(prediction, probability)),
             columns=["prediction", "probability"],
@@ -67,16 +71,19 @@ def run_extraction(corpus_embeddings, corpus_labels, corpus_ids, training_ids):
             if row.prediction != row.next:
                 prob = df.loc[start_idx:idx].probability.mean()
                 end_idx = idx + 1
-                predictions_with_probabilities.append([float(prob), row.prediction, start_idx, end_idx])
+                predictions_with_probabilities.append(
+                    [float(prob), row.prediction, start_idx, end_idx]
+                )
                 new_start_idx = True
         ml_results_by_record_id[record_id] = predictions_with_probabilities
     if len(ml_results_by_record_id) == 0:
         print("No records were predicted. Try lowering the confidence threshold.")
+    truss.mk_truss(extractor.model, target_directory=TRUSS_DIR)
     return ml_results_by_record_id
 
 
 if __name__ == "__main__":
-    _, payload_url = sys.argv
+    _, payload_url, model_name_url = sys.argv
     print("Preparing data for machine learning.")
 
     corpus_embeddings, corpus_labels, corpus_ids, training_ids = util.get_corpus()
@@ -95,3 +102,6 @@ if __name__ == "__main__":
 
     print("Finished execution.")
     requests.put(payload_url, json=ml_results_by_record_id)
+
+    shutil.make_archive("model", "zip", TRUSS_DIR)
+    requests.put(model_name_url, files={"file": open("model.zip", "rb")})
